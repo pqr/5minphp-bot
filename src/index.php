5d794062c042aa00e94f7668027cd866f5adb3b7
Petr Myazin <petr.myazin@gmail.com>
2016-11-14 13:38:55 +0300

    add rollbar error handling

--- src/index.php
+++ src/index.php
@@ -1,6 +1,17 @@
 <?php
+require_once __DIR__ . '/rollbar.php';
+// set-up error handling via rollbar
+Rollbar::init([
+    // required
+    'access_token' => '916b71816d754c3abd77b300d4d9941c',
+    // sets whether errors suppressed with '@' should be reported or not
+    'report_suppressed' => true,
+    // optional - path to directory your code is in. used for linking stack traces.
+    'root' => __DIR__
+]);
+
 define('LOCAL_FILE', __DIR__ . '/phpfact.txt');
-define('REMOTE_FILE', 'https://raw.githubusercontent.com/pqr/5minphp-bot/master/phpfact.txt');
+define('REMOTE_FILE', 'https://raw.githubusercontent.com/pqr/5minphp-bot/master/phpfact.txt1');
 define('BOT_NAME', 'Пятиминутка PHP');
 
 if (isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] === '/info') {
@@ -19,7 +30,7 @@ if (!$input) {
     exit();
 }
 
-$inputData = json_decode($input, true);
+$inputData = @json_decode($input, true);
 if (!$inputData || !isset($inputData['text']) || !is_string($inputData['text'])) {
     http_response_code(417);
     exit();
@@ -27,6 +38,7 @@ if (!$inputData || !isset($inputData['text']) || !is_string($inputData['text']))
 
 if (mb_stripos($inputData['text'], 'пятиминутка, обновись') !== false) {
     http_response_code(201);
+    downloadFacts();
     print json_encode(['text' => 'У меня для вас есть свежие факты про PHP. Просто напишите сообщение с текстом содержащим "PHP", и вы узнаете, что...', 'bot' => BOT_NAME]);
     exit();
 }
@@ -61,7 +73,7 @@ function hasWordPhp($inputText): bool
 
 function getRandomFact(string $inputData): string
 {
-    if (!file_exists(LOCAL_FILE) || time() - @filemtime(LOCAL_FILE) > 3600) {
+    if (!@file_exists(LOCAL_FILE) || time() - @filemtime(LOCAL_FILE) > 3600) {
         downloadFacts();
     }
 
